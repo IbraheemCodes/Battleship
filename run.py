1a9bc5a6110790fb4fc1099eeee9d5b50bb40515
IbraheemCodes <>
2023-11-12 17:19:34 +0000

    added class and enums

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import gspread
 from google.oauth2.service account import Credentials
+from enum import Enum
+import random
+
 
 
 SCOPE = [
@@ -10,3 +13,29 @@ SCOPE = [
 
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
+
+# Battleship
+
+# Variables and enums
+class Icons(Enum):
+    SHIP = "🚢"
+    FLAME = "🔥"
+    WATER = "🌊"
+    ROBOT = "🤖"
+    COOL = "😎"
+    EXPLOSION = "💥"
+    BARREL = "🛢️"
+    BARRELS = "🛢️🛢️🛢️🛢️🛢️"
+    TELESCOPE = "🔭"
+    GUN = "🔫"
+    WOMAN_SMOKING = "🚬👩"
+
+class Difficulty(Enum):
+    STANDARD = 1  # Default
+    VETERAN = 2
+    NEAR_IMPOSSIBLE = 3
+    
+class State(Enum):
+    INITIALISING = 1
+    ACTIVE = 2
+    AWAITING_END_ACTION = 3
