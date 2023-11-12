d2c26ae4520860b97aee6570756c5e8ff7087a4d
IbraheemCodes <>
2023-11-12 17:29:13 +0000

    added game class

--- run.py
+++ run.py
@@ -30,12 +30,30 @@ class Icons(Enum):
     GUN = "🔫"
     WOMAN_SMOKING = "🚬👩"
 
+
 class Difficulty(Enum):
     STANDARD = 1  # Default
     VETERAN = 2
     NEAR_IMPOSSIBLE = 3
     
+  
 class State(Enum):
     INITIALISING = 1
     ACTIVE = 2
     AWAITING_END_ACTION = 3
+
+
+# Game class
+class Game:
+    def __init__(self, opponent_difficulty, game_state):
+        self.opponent_difficulty = opponent_difficulty
+        self.game_state = game_state
+        
+    def set_difficulty(self, difficulty):
+        self.opponent_difficulty = difficulty
+        
+    def set_state(self, state):
+        self.game_state = state
+        
+    def get_difficulty(self):
+        return self.opponent_difficulty
